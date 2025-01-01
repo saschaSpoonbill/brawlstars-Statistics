5d51e85d402b9529e6a1a9770b8b15198f997bf7
saschaSpoonbill <sascha_loeffler@gmx.de>
2025-01-01 16:58:11 +0100

    Updated Analytics

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import logging
 import plotly.express as px
 import together
 import json
+import pathlib
+from bs4 import BeautifulSoup
+import shutil
 
 # Set page config must be the first Streamlit command
 st.set_page_config(
@@ -16,21 +19,38 @@ st.set_page_config(
     layout="wide"
 )
 
-# Google Analytics Code
-GA_CODE = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NCG7739DG5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+def inject_ga():
+    """Inject Google Analytics code into Streamlit's index.html"""
+    GA_ID = "google_analytics"
 
-  gtag('config', 'G-NCG7739DG5');
-</script>
-"""
+    GA_JS = """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NCG7739DG5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NCG7739DG5');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
 
 # Inject Google Analytics
-components.html(GA_CODE, height=0)
+inject_ga()
 
 from api_client import BrawlStarsAPI
 from data_processor import BrawlStarsDataProcessor
